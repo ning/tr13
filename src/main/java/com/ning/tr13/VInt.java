0e60318dfa2ffda0e5d660024fb5c8a48cdcdf0c
Tatu <tatu@ning.com>
2010-05-18 22:23:34 -0700

    Fixed VInt code, added decoding, added unit tests.

--- src/main/java/com/ning/tr13/VInt.java
+++ src/main/java/com/ning/tr13/VInt.java
@@ -1,5 +1,11 @@
 package com.ning.tr13;
 
+/**
+ * Helper class for dealing with Variable-length integers (VInts).
+ * Encoding uses big-endian (most-significant byte first) ordering;
+ * number of bits in the first byte can vary to allow using couple of
+ * leading bits for type marking.
+ */
 public class VInt
 {
     /**
@@ -29,15 +35,173 @@ public class VInt
      */
     public static int unsignedToBytes(long value, int bitsForFirstByte, byte[] result, int offset)
     {
-        // note: 1 bit is 'stolen' from each bit, to use as continuation
-        bitsForFirstByte--;        
-        int mask = 0xFF >> (8 - bitsForFirstByte);
-        result[offset++] = (byte) (((int) value) & mask);
-        value  = (value >>> bitsForFirstByte);
-        while (value != 0L) {
-            result[offset++] = (byte) value;
-            value >>= 7;
+        // N bits fit in the first byte; but MSB is the 'last byte' marker bit:
+        --bitsForFirstByte;
+        int mask = (1 << bitsForFirstByte) - 1;
+        // Does value fit in that one byte?
+        // so to reserve room for it
+        if ((value & mask) == value) { // yup; single byte value:
+            int i = (int) value;
+            result[offset] = (byte) ((i & mask) | (1 << bitsForFirstByte));
+            return offset+1;
         }
+        // Ok: one more special case; if 4 bytes are enough, can do more efficient handling:
+        if ((value >> (21 + bitsForFirstByte)) == 0L) {
+            return _unsignedIntToBytes((int) value, result, offset, bitsForFirstByte, mask);
+        }
+        // If not, can quickly determine 4 LSBs        
+        int i = (int) value;
+        byte b0 = (byte) ((i & 0x7F) | 0x80);
+        i >>= 7;
+        byte b1 = (byte) (i & 0x7F);
+        i >>= 7;
+        byte b2 = (byte) (i & 0x7F);
+        i >>= 7;
+        byte b3 = (byte) (i & 0x7F);
+        value >>>= 28;
+
+        if (value <= mask) {
+            return _appendBytes(result, offset, (byte) value, b3, b2, b1, b0);
+        }
+        byte b4 = (byte) (value & 0x7F);
+
+        // Ok: 5 bytes (== 35 bits) done; can safely revert back to int now
+        i = (int) (value >> 7);
+        if (i <= mask) {
+            result[offset++] = (byte) i;
+            return _appendBytes(result, offset, b4, b3, b2, b1, b0);
+        }
+        byte b5 = (byte) (i & 0x7F);
+        i >>= 7;
+        if (i <= mask) { // 7 bytes
+            result[offset++] = (byte) i;
+            result[offset++] = b5;
+            return _appendBytes(result, offset, b4, b3, b2, b1, b0);
+        }
+        byte b6 = (byte) (i & 0x7F);
+        i >>= 7;
+        if (i <= mask) { // 8 bytes
+            result[offset++] = (byte) i;
+            result[offset++] = b6;
+            result[offset++] = b5;
+            return _appendBytes(result, offset, b4, b3, b2, b1, b0);
+        }
+        byte b7 = (byte) (i & 0x7F);
+        i >>= 7;
+        if (i <= mask) { // 9 bytes
+            result[offset++] = (byte) i;
+            result[offset++] = b7;
+            result[offset++] = b6;
+            result[offset++] = b5;
+            return _appendBytes(result, offset, b4, b3, b2, b1, b0);
+        }
+        // unlikely, but possible; up to 10 bytes; 9 with 7 bits, one with N <= 7 bits
+        byte b8 = (byte) (i & 0x7F);
+        i >>= 7;
+        offset = _appendBytes(result, offset, (byte) i, b8, b7, b6, b5);
+        return _appendBytes(result, offset, b4, b3, b2, b1, b0);
+    }
+
+    /**
+     * Reverse of {@link #unsignedToBytes}
+     * 
+     * @param bitsForFirstByte Number of (least-significant) data bits in the first byte
+     * @param buffer Buffer that contains bytes (caller has to ensure all data bytes are
+     *   included)
+     * @param offset Offset of the first data byte in buffer
+     * 
+     * @result Offset of the first byte following decoded long
+     */
+    public static int bytesToUnsigned(int bitsForFirstByte, byte[] buffer, int offset,
+            long[] resultBuffer)
+    {
+        // Ok: first byte has N bits, of which MSB indicates if it's the last byte to use:
+        int value = buffer[offset++] & ((1 << bitsForFirstByte) - 1);
+        bitsForFirstByte--;
+        int marker = (1 << bitsForFirstByte);
+        if ((value & marker) != 0) { // if we have MSB set, it means 'last byte'
+            value ^= marker;
+            resultBuffer[0] = value;
+            return offset;
+        }
+        value <<= 7;
+        // Otherwise we'll just get N LSB
+        int b = buffer[offset++];
+        if (b < 0) { // 2 bytes
+            resultBuffer[0] = value | (b & 0x7F);
+            return offset;
+        }
+        value = (value | b) << 7;
+        b = buffer[offset++];
+        if (b < 0) { // 3 bytes
+            resultBuffer[0] = value | (b & 0x7F);
+            return offset;
+        }
+        value = (value | b) << 7;
+        b = buffer[offset++];
+        if (b < 0) { // 4 bytes
+            resultBuffer[0] = value | (b & 0x7F);
+            return offset;
+        }
+        value = (value | b);
+        // At this point we must 'upgrade' to long; can just loop
+        long l = (long) value;
+        while (true) {
+            l <<= 7;
+            b = buffer[offset++];
+            if (b < 0) { // last byte
+                resultBuffer[0] = l | (b & 0x7F);
+                return offset;
+            }
+            l |= b;
+        }
+    }
+
+    /*
+    /**********************************************************
+    /* Helper methods
+    /**********************************************************
+     */
+
+    /**
+     * Helper method called when VInt fits within 4 bytes
+     */
+    private static int _unsignedIntToBytes(int value, byte[] result, int offset,
+            int bitsForFirstByte, int maskForFirstByte)
+    {
+        // Ok: we can figure out 3 more bytes just from lower int:
+        byte b0 = (byte) ((value & 0x7F) | 0x80);
+        value >>= 7;
+        if (value <= maskForFirstByte) {
+            result[offset++] = (byte) value;
+            result[offset++] = b0;
+            return offset;
+        }
+        byte b1 = (byte) (value & 0x7F);
+        value >>= 7;
+        if (value <= maskForFirstByte) {
+            result[offset++] = (byte) value;
+            result[offset++] = b1;
+            result[offset++] = b0;
+            return offset;
+        }
+        result[offset++] = (byte) (value >> 7);
+        result[offset++] = (byte) (value & 0x7F);
+        result[offset++] = b1;
+        result[offset++] = b0;
         return offset;
     }
+
+    private static int _appendBytes(byte[] buffer, int ptr,
+            byte b0, byte b1, byte b2, byte b3, byte b4)
+    {
+        buffer[ptr++] = b0;
+        buffer[ptr++] = b1;
+        buffer[ptr++] = b2;
+        buffer[ptr++] = b3;
+        buffer[ptr++] = b4;
+        return ptr;
+    }
+    
+    
 }
--- src/test/java/com/ning/tr13/VIntTest.java
+++ src/test/java/com/ning/tr13/VIntTest.java
@@ -3,7 +3,7 @@ package com.ning.tr13;
 public class VIntTest
     extends junit.framework.TestCase
 {
-    public void testDummy() throws Exception
+    public void testLengthCalculation() throws Exception
     {
         // note: 8 means 8 available data bits; but one bit is needed for indicating where it's last byte
         assertEquals(1, VInt.lengthForUnsigned(0x1, 8));
@@ -11,5 +11,49 @@ public class VIntTest
         assertEquals(2, VInt.lengthForUnsigned(0x80, 8));
         assertEquals(2, VInt.lengthForUnsigned(0x3FFF, 8));
         assertEquals(3, VInt.lengthForUnsigned(0x4000, 8));
+
+        // If we had 'pure' VInts (7 bits for each byte), max positive int would only take 9 bytes
+        assertEquals(9, VInt.lengthForUnsigned(Long.MAX_VALUE, 8));
+        // and since we claim to pass unsigned, -1 is actually 64 bits of payload, so:
+        assertEquals(10, VInt.lengthForUnsigned(-1L, 8));
+    }
+
+    public void testRoundTrip() throws Exception
+    {
+        // first, small values
+        _assertRoundtrip(0L);
+        _assertRoundtrip(1L);
+        _assertRoundtrip(127L);
+        _assertRoundtrip(128L);
+        _assertRoundtrip(-1L);
+        _assertRoundtrip(-127L);
+        _assertRoundtrip(-255L);
+
+        // then, single bit values (0x2,0x4, ... 0x80000000)
+        for (int bit = 1; bit < 64; ++bit) {
+            long value = 1L << bit;
+            _assertRoundtrip(value);
+        }
+        // and 'all ones'
+        for (long value = 0x3; value != -1L; value = (value << 1) + 1) {
+            _assertRoundtrip(value);            
+        }
+    }
+
+    private void _assertRoundtrip(long value)
+    {
+        final int BITS_FOR_FIRST_BYTE = 6;
+        // up to 10 bytes for encoded
+        byte[] buffer = new byte[11];
+        long[] resultLong = new long[1];
+        int ptr = VInt.unsignedToBytes(value, BITS_FOR_FIRST_BYTE, buffer, 1);
+        // ensure that first 2 bits are clear
+        assertEquals(0, buffer[1] & 0xC0);
+        // and that length is what expected length would look for
+        int len = ptr - 1;
+        assertEquals(len, VInt.lengthForUnsigned(value, BITS_FOR_FIRST_BYTE));
+        int resultOffset = VInt.bytesToUnsigned(BITS_FOR_FIRST_BYTE, buffer, 1, resultLong);
+        assertEquals(ptr, resultOffset);
+        assertEquals(value, resultLong[0]);
     }
 }
